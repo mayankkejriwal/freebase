0346a0f7ca88d7922c1ba53e181d4ea39d0b1bd8
mayankkejriwal <mayankkejriwal@utexas.edu>
2015-10-07 12:03:40 -0500

    reorganize Curate.java to make it more usable

--- src/datasetDetails/Curate.java
+++ src/datasetDetails/Curate.java
@@ -7,8 +7,8 @@ public class Curate {
 
 	
 	public static String path="C:\\Users\\Mayank\\SkyDrive\\Documents\\"
-			+ "experiments\\trash\\"; 
-	static String file=path+"dbpediaFreebaseYagoSameAsAppend.crdownload";
+			+ "experiments\\freebase\\derived-data-files\\"; 
+	static String file=path+"consolidatedGlobalTokens2";
 	
 	public static void main(String[] args)throws IOException {
 		//testDbpediaDelimiter(15);
@@ -19,15 +19,68 @@ public class Curate {
 		//checkCounts();
 		//printFirstLastID(file);
 		
-		
+		analysis(file);
 		
 		//filterRankedFile(file,path+"f-db-jaccard-top3.txt",3);
 		//debugCuratedInstanceTypes();
-		printN(10);
+	//	printN(10);
 		//printNumLines();
 		//curateLinksFileYago();
 
 	}
+	
+	/*
+	 * Use this function as a 'placeholder' analysis. Rather than wading
+	 * through all the code to find a proper analysis function, just write
+	 * a few short lines in here.
+	 */
+	public static void analysis(String file)throws IOException{
+		Scanner in=new Scanner(new FileReader(file));
+		while(in.hasNextLine()){
+			String line=in.nextLine();
+			
+			String[] fields=line.split("\t");
+			if(fields.length>60001)
+				System.out.println("field size "+fields.length);
+		}
+		in.close();
+	}
+	
+	//combine two consolidatedGlobalTokens files into one. Note that number of tokens
+	//could now be more than 30,000 per type. 
+	public static void combineConsolidatedFiles()throws FileNotFoundException{
+		String file1=file+"1";
+		String file2=file+"1";
+		String outfile=path+"consolidatedCombined12";
+		HashMap<String, HashMap<String,String>> dict=new HashMap<String, HashMap<String,String>>(4000);
+		Scanner in=new Scanner(new FileReader(file1));
+		while(in.hasNextLine()){
+			String[] t=in.nextLine().split("\t");
+			dict.put(t[0], buildDataStructure(t));
+		}
+		in.close();
+		in=new Scanner(new FileReader(file2));
+		
+		PrintWriter out=new PrintWriter(new File(outfile));
+		while(in.hasNextLine()){
+			String[] t=in.nextLine().split("\t");
+			if(dict.containsKey(t[0])){
+				out.println(t[0]+"\t"+combine(dict.get(t[0]),buildDataStructure(t)));
+				dict.remove(t[0]);
+			}
+			else
+				
+				out.println(t[0]+"\t"+combine(buildDataStructure(t), null));
+			
+				
+		}
+		in.close();
+		for(String k: dict.keySet()){
+			out.println(k+"\t"+combine(dict.get(k), null));
+		}
+		out.close();
+	}
+
 	/*
 	 * takes a f-db-*-ranked.txt file, produces an output file that only
 	 * contains those lines with rank less than equal to 'rank'.
@@ -43,57 +96,6 @@ public class Curate {
 		
 		in.close();
 		out.close();
-	}
-	
-	public static void debugCuratedInstanceTypes()throws IOException{
-		Scanner in=new Scanner(new FileReader(file));
-		String check="><";
-		while(in.hasNextLine()){
-			String line=in.nextLine();
-			if(line.contains(check))
-				System.out.println(line);
-		}
-		in.close();
-		
-	}
-	
-	//meant for those files (for example f-db-*.txt or curated_freebase_links)
-	//will print out id in the first and last lines. Treat with caution, interpretations
-	//could vary. Will also print out the number of lines in the file, for reference.
-	public static void printFirstLastID(String file)throws IOException{
-		Scanner in=new Scanner(new FileReader(file));
-		String firstID=new String();
-		String lastID=new String();
-		if(in.hasNextLine())
-			firstID=in.nextLine().split("\t")[0];
-		long count=1;
-		while(in.hasNextLine()){
-			String line=in.nextLine();
-			count++;
-			lastID=line.split("\t")[0];
-		}
-		
-		in.close();
-		System.out.println("Lines in file: "+count);
-		System.out.println("First ID in file: "+firstID);
-		System.out.println("Last ID in file: "+lastID);
-	}
-	
-	/**
-	 * Meant to test combine and buildDataStructure since we are using them in
-	 * a MapReduce job.
-	 */
-	public static void testCombineAndBuildDS(){
-		String f="type\tstring1\t2\tstring2\t4\tstring3\t6";
-		String g="type\tstring1\t7\tstring5\t4\tstring3\t9";
-		String h="type\tstring1\t7";
-		printHashMap(buildDataStructure(f.split("\t")));
-		ArrayList<String> k=new ArrayList<String>();
-		k.add(f);
-		k.add(g);
-		k.add(h);
-		System.out.println(combine(k));
-		//System.out.println(combine(buildDataStructure(f.split("\t")),null));
 	}
 	
 	/*
@@ -136,66 +138,7 @@ public class Curate {
 		
 	}
 	
-	//combine two consolidatedGlobalTokens files into one. Note that number of tokens
-	//could now be more than 30,000 per type. 
-	public static void combineConsolidatedFiles()throws FileNotFoundException{
-		String file1=file+"1";
-		String file2=file+"1";
-		String outfile=path+"consolidatedCombined12";
-		HashMap<String, HashMap<String,String>> dict=new HashMap<String, HashMap<String,String>>(4000);
-		Scanner in=new Scanner(new FileReader(file1));
-		while(in.hasNextLine()){
-			String[] t=in.nextLine().split("\t");
-			dict.put(t[0], buildDataStructure(t));
-		}
-		in.close();
-		in=new Scanner(new FileReader(file2));
-		
-		PrintWriter out=new PrintWriter(new File(outfile));
-		while(in.hasNextLine()){
-			String[] t=in.nextLine().split("\t");
-			if(dict.containsKey(t[0])){
-				out.println(t[0]+"\t"+combine(dict.get(t[0]),buildDataStructure(t)));
-				dict.remove(t[0]);
-			}
-			else
-				
-				out.println(t[0]+"\t"+combine(buildDataStructure(t), null));
-			
-				
-		}
-		in.close();
-		for(String k: dict.keySet()){
-			out.println(k+"\t"+combine(dict.get(k), null));
-		}
-		out.close();
-	}
-	
-	/*
-	 * Built the appropriate data structure from the tab-delimited array. Remember
-	 * that the first element (a type) should be ignored.
-	 */
-	public static HashMap<String, String> buildDataStructure(String[] tokens){
-		HashMap<String, String> result=new HashMap<String, String>(30000);
-		for(int i=1; i<tokens.length; i+=2){
-			result.put(tokens[i], tokens[i+1]);
-		}
-		
-		return result;
-	}
-	
-	//sets must contain at least one set, otherwise null pointer exception
-	public static <T> HashSet<T> union(ArrayList<HashSet<T>> sets){
-		HashSet<T> big=new HashSet<T>(sets.get(0));
-		for(int i=1; i<sets.size(); i++){
-			HashSet<T> tmp=sets.get(i);
-			for(T t: tmp)
-				big.add(t);
-		}
-		return big;
-	}
-	
-	public static void checkTypeCounts()throws FileNotFoundException{
+	public static void printTypeCounts()throws FileNotFoundException{
 		Scanner in=new Scanner(new FileReader(file));
 		int count=0;
 		int total=0;
@@ -211,6 +154,34 @@ public class Curate {
 		System.out.println("dbpedia.org/ontology types: "+count);
 	}
 	
+	public static <T,P> void printHashMap(HashMap<T, P> map){
+		for(T t: map.keySet()){
+			System.out.println(t.toString()+"\t"+map.get(t).toString());
+		}
+	}
+
+	//meant for those files (for example f-db-*.txt or curated_freebase_links)
+	//will print out id in the first and last lines. Treat with caution, interpretations
+	//could vary. Will also print out the number of lines in the file, for reference.
+	public static void printFirstLastID(String file)throws IOException{
+		Scanner in=new Scanner(new FileReader(file));
+		String firstID=new String();
+		String lastID=new String();
+		if(in.hasNextLine())
+			firstID=in.nextLine().split("\t")[0];
+		long count=1;
+		while(in.hasNextLine()){
+			String line=in.nextLine();
+			count++;
+			lastID=line.split("\t")[0];
+		}
+		
+		in.close();
+		System.out.println("Lines in file: "+count);
+		System.out.println("First ID in file: "+firstID);
+		System.out.println("Last ID in file: "+lastID);
+	}
+
 	public static void printN(int n) throws FileNotFoundException{
 		Scanner in=new Scanner(new FileReader(file));
 		int count=0;
@@ -231,37 +202,28 @@ public class Curate {
 		in.close();
 	}
 	
-	//print for first n lines
-	public static void testDbpediaDelimiter(int n)throws IOException{
+	/**
+	 * Prints the number of lines in the file
+	 * 
+	 * @throws FileNotFoundException
+	 */
+	public static void printNumLines() throws FileNotFoundException{
 		Scanner in=new Scanner(new FileReader(file));
 		int count=0;
-		while(in.hasNextLine() && count<n){
-			if(in.nextLine().substring(0,1).equals("#"))
-				continue;
-			String[] fields=in.nextLine().split("> ");
-			if(fields.length!=3&&fields.length!=4)
-				continue;
-			System.out.print(fields.length+"\t");
-			printArray(fields);
+		while(in.hasNextLine()){
+			in.nextLine();
 			count++;
 		}
-		
 		in.close();
+		System.out.println("Total number of lines: "+count);
 	}
-	
-	public static void testReplace(){
-		String f="'''<3542345>";
-		f=f.replaceAll("'", "");
-		f=f.replaceAll("<", "");
-		f=f.replaceAll(">", "");
-		try{
-			int k=Integer.parseInt(f);
-			System.out.println(k);
-		}catch(NumberFormatException e){
-			
-		}
-		System.out.println(f);
+
+	public static void printArray(String[] fields){
+		for(String field: fields)
+			System.out.println(field);
+		System.out.println();
 	}
+
 	/**
 	 * We'll take the freebase_links.nt file, then print out another file that:
 	 * (1) Only contains lower-case elements
@@ -343,18 +305,31 @@ public class Curate {
 		in.close();
 		out.close();
 	}
-	public static <T,P> void printHashMap(HashMap<T, P> map){
-		for(T t: map.keySet()){
-			System.out.println(t.toString()+"\t"+map.get(t).toString());
+	//sets must contain at least one set, otherwise null pointer exception
+	private static <T> HashSet<T> union(ArrayList<HashSet<T>> sets){
+		HashSet<T> big=new HashSet<T>(sets.get(0));
+		for(int i=1; i<sets.size(); i++){
+			HashSet<T> tmp=sets.get(i);
+			for(T t: tmp)
+				big.add(t);
 		}
+		return big;
 	}
-	
-	private static void printArray(String[] fields){
-		for(String field: fields)
-			System.out.println(field);
-		System.out.println();
+
+	/*
+	 * Built the appropriate data structure from the tab-delimited array. Remember
+	 * that the first element (a type) should be ignored.
+	 */
+	private static HashMap<String, String> buildDataStructure(String[] tokens){
+		HashMap<String, String> result=new HashMap<String, String>(30000);
+		for(int i=1; i<tokens.length; i+=2){
+			result.put(tokens[i], tokens[i+1]);
+		}
+		
+		return result;
 	}
-	public static HashMap<String,String> combine(HashMap<String, String> a, HashMap<String, String> b){
+
+	private static HashMap<String,String> combine(HashMap<String, String> a, HashMap<String, String> b){
 		HashMap<String, String> result=new HashMap<String, String>();
 		for(String k: a.keySet()){
 			
@@ -372,34 +347,6 @@ public class Curate {
 			
 		}
 		return result;
-	}
-	public static HashMap<String,String> combine(ArrayList<String> list){
-		if(list.size()<=1)
-			return buildDataStructure(list.get(0).split("\t"));
-		
-		HashMap<String,String> result=combine(buildDataStructure(list.get(0).split("\t")),buildDataStructure(list.get(1).split("\t")));
-		for(int i=2; i<list.size(); i++){
-			result=combine(result,buildDataStructure(list.get(i).split("\t")));
-		}
-		
-		
-		return result;
-	}
-
-	/**
-	 * Prints the number of lines in the file
-	 * 
-	 * @throws FileNotFoundException
-	 */
-	public static void printNumLines() throws FileNotFoundException{
-		Scanner in=new Scanner(new FileReader(file));
-		int count=0;
-		while(in.hasNextLine()){
-			in.nextLine();
-			count++;
-		}
-		in.close();
-		System.out.println("Total number of lines: "+count);
 	}
 
 }
