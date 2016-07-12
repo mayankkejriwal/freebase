303f5e51e74e0a792dbf243800bbc604700e737c
mayankkejriwal <mayankkejriwal@utexas.edu>
2016-07-11 17:12:43 -0700

    Added new feature maps to parseJSONintoStringFeatures

--- src/developmentSet/DevelopmentSet.java
+++ src/developmentSet/DevelopmentSet.java
@@ -288,6 +288,129 @@ public class DevelopmentSet {
 		return result;
 	}
 	
+	/**
+	 * This function was taken from : http://grepcode.com/file_/repo1.maven.org/maven2/org.apache.commons/
+	 * commons-lang3/3.0/org/apache/commons/lang3/math/NumberUtils.java/?v=source
+	 * 
+	 * The only thing I changed was the first if condition, since it
+	 * was using StringUtils.
+	 * 
+     * <p>Checks whether the String a valid Java number.</p>
+     *
+     * <p>Valid numbers include hexadecimal marked with the <code>0x</code>
+     * qualifier, scientific notation and numbers marked with a type
+     * qualifier (e.g. 123L).</p>
+     *
+     * <p><code>Null</code> and empty String will return
+     * <code>false</code>.</p>
+     *
+     * @param str  the <code>String</code> to check
+     * @return <code>true</code> if the string is a correctly formatted number
+     */
+    private static boolean isNumber(String str) {
+        if (str==null||str.length()==0) {
+            return false;
+        }
+        char[] chars = str.toCharArray();
+        int sz = chars.length;
+        boolean hasExp = false;
+        boolean hasDecPoint = false;
+        boolean allowSigns = false;
+        boolean foundDigit = false;
+        // deal with any possible sign up front
+        int start = (chars[0] == '-') ? 1 : 0;
+        if (sz > start + 1) {
+            if (chars[start] == '0' && chars[start + 1] == 'x') {
+                int i = start + 2;
+                if (i == sz) {
+                    return false; // str == "0x"
+                }
+                // checking hex (it can't be anything else)
+                for (; i < chars.length; i++) {
+                    if ((chars[i] < '0' || chars[i] > '9')
+                        && (chars[i] < 'a' || chars[i] > 'f')
+                        && (chars[i] < 'A' || chars[i] > 'F')) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        sz--; // don't want to loop to the last char, check it afterwords
+              // for type qualifiers
+        int i = start;
+        // loop to the next to last char or to the last char if we need another digit to
+        // make a valid number (e.g. chars[0..5] = "1234E")
+        while (i < sz || (i < sz + 1 && allowSigns && !foundDigit)) {
+            if (chars[i] >= '0' && chars[i] <= '9') {
+                foundDigit = true;
+                allowSigns = false;
+
+            } else if (chars[i] == '.') {
+                if (hasDecPoint || hasExp) {
+                    // two decimal points or dec in exponent   
+                    return false;
+                }
+                hasDecPoint = true;
+            } else if (chars[i] == 'e' || chars[i] == 'E') {
+                // we've already taken care of hex.
+                if (hasExp) {
+                    // two E's
+                    return false;
+                }
+                if (!foundDigit) {
+                    return false;
+                }
+                hasExp = true;
+                allowSigns = true;
+            } else if (chars[i] == '+' || chars[i] == '-') {
+                if (!allowSigns) {
+                    return false;
+                }
+                allowSigns = false;
+                foundDigit = false; // we need a digit after the E
+            } else {
+                return false;
+            }
+            i++;
+        }
+        if (i < chars.length) {
+            if (chars[i] >= '0' && chars[i] <= '9') {
+                // no type qualifier, OK
+                return true;
+            }
+            if (chars[i] == 'e' || chars[i] == 'E') {
+                // can't have an E at the last byte
+                return false;
+            }
+            if (chars[i] == '.') {
+                if (hasDecPoint || hasExp) {
+                    // two decimal points or dec in exponent
+                    return false;
+                }
+                // single trailing decimal point after non-exponent is ok
+                return foundDigit;
+            }
+            if (!allowSigns
+                && (chars[i] == 'd'
+                    || chars[i] == 'D'
+                    || chars[i] == 'f'
+                    || chars[i] == 'F')) {
+                return foundDigit;
+            }
+            if (chars[i] == 'l'
+                || chars[i] == 'L') {
+                // not allowing L with an exponent or decimal point
+                return foundDigit && !hasExp && !hasDecPoint;
+            }
+            // last character is illegal
+            return false;
+        }
+        // allowSigns is true iff the val ends in 'E'
+        // found digit it to make sure weird stuff like '.' and '1E-' doesn't pass
+        return !allowSigns && foundDigit;
+    }
+	
 	
 	/**
 	 * ArrayList will contain exactly k hashmaps. Each map is a token together with
@@ -301,12 +424,24 @@ public class DevelopmentSet {
 	 * Third hashmap contains non-property and non-literal tokens (O).
 	 * Fourth hashmap contains the union of the three hashmaps (with summation over
 	 * common elements) (A)
+	 * 
+	 * I'm adding the next three hashmaps on 11th July 2016:
+	 * 
+	 * Fifth hashmap: applies a numeric only filter to A (N)
+	 * Sixth hashmap: subset of A, only contains tokens from fields containing
+	 * five tokens or fewer (Sh)
+	 * Seventh hashmap: A - Sh
+	 * 
+	 * I haven't tested the new hashmaps yet!
 	 */
 	public static ArrayList<HashMap<String, Integer>> parseJSONIntoStringFeatures(String[] json){
-		ArrayList<HashMap<String, Integer>> result=new ArrayList<HashMap<String, Integer>>(3);
+		ArrayList<HashMap<String, Integer>> result=new ArrayList<HashMap<String, Integer>>(7);
 		result.add(new HashMap<String,Integer>());
 		result.add(new HashMap<String,Integer>());
 		result.add(new HashMap<String,Integer>());
+		
+		HashMap<String,Integer> Sh=new HashMap<String,Integer>();
+		HashMap<String,Integer> Lo=new HashMap<String,Integer>();
 		
 		for(String propValues: json){
 			String[] attributes=propValues.split("\":\\[");
@@ -333,13 +468,25 @@ public class DevelopmentSet {
 				result.get(0).put(propToken, g+1);
 			}
 			
-			//now let's deal with L and O tokens
+			//now let's deal with L, Sh and O tokens
 			String values=attributes[1];
 			
 			
 			
 			values=values.replaceAll("\\]","");
 			String[] list=values.split("\", \"");
+			if(list.length<=5)
+				for(String element: list){
+					if(!Sh.containsKey(element))
+						Sh.put(element, 0);
+					Sh.put(element, Sh.get(element)+1);
+				}
+			else
+				for(String element: list){
+					if(!Lo.containsKey(element))
+						Lo.put(element, 0);
+					Lo.put(element, Lo.get(element)+1);
+				}
 			for(int j=0; j<list.length; j++){
 				//is it a literal?
 				boolean literal=false;
@@ -374,7 +521,7 @@ public class DevelopmentSet {
 				}
 			}
 		}
-		//finally, we'll deal with A tokens. Note that this has not been added yet.
+		//Now, we'll deal with A tokens. Note that this has not been added yet.
 		HashMap<String,Integer> tmp=new HashMap<String,Integer>();
 		for(int i=0; i<result.size(); i++){
 			HashMap<String,Integer> k=result.get(i);
@@ -386,6 +533,20 @@ public class DevelopmentSet {
 			}
 		}
 		result.add(tmp);
+		
+		//Now, we'll deal with N tokens
+		HashMap<String,Integer> N=new HashMap<String,Integer>();
+		
+		for(String k:tmp.keySet()){
+			if(isNumber(k))
+				N.put(k, tmp.get(k));
+			
+		}
+		result.add(N);
+		result.add(Sh);
+		result.add(Lo);
+		
+		
 		return result;
 	}
 	
